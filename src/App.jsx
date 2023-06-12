f90fdf2be902daf15ea677187a72f92f00ab9ff0
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-06-12 14:57:42 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Nav from './components/Nav/Nav'
+
+function App () {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Nav />
+    </div>
+  )
+}
+
+export default App
